f385e167e6c345a0bb9355e89a820c9318e0610f
Jagunmolu <kolawolejonathan@gmail.com>
2023-12-08 23:53:38 +0100

    added the first R file

---
+++ day_ten/intro_to_r.R
@@ -0,0 +1,11 @@
+# this is a comment in R
+# declaring a variable
+my.name <- "Jagunmolu"
+# printing to the console
+print(my.name)
+# declaring a vector
+vec <- 1:20
+# a matrix
+mat <- matrix(data = vec, nrow = 4, byrow = TRUE)
+# vector variable types include:
+#logical, raw, complex, character, double, and integer
